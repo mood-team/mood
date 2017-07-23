77f98593691fcf4424915413f7e177ce4ce932b3
simran-arora <emailsimran@gmail.com>
2017-07-22 21:38:43 -0700

    Added edits.

--- flickrApp1/Mood/CreatorViewController.swift
+++ flickrApp1/Mood/CreatorViewController.swift
@@ -57,6 +57,36 @@ class CreatorViewController: UIViewController, UITextFieldDelegate, UIImagePicke
         topText.resignFirstResponder()
         middleText.resignFirstResponder()
         bottomText.resignFirstResponder()
+        
+        // allows photos to be picked but not taken
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        
+        // notify ViewController when the user picks an image
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // dismiss from picker if the user clicks cancel
+        dismiss(animated: true, completion:nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        //photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
     
 }
